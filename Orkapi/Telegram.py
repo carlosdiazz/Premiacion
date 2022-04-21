a93a264dd706e3768940545fa86db338ac14c825
carlosdiazz <c.diazadriann@gmail.com>
2022-04-20 21:20:36 -0400

    ROBOT Listo

--- Orkapi/Telegram.py
+++ Orkapi/Telegram.py
@@ -19,12 +19,20 @@ TOKEN = '5348496240:AAHvkD64i5AveuGv3v_5K1y5AyPn74MOqVg'
 COMANDOS = [
     '/Start',
     "/Info",
+    "/Premiar",
+    "/Ver_Resultados"
+]
+
+Comandos_Premios =[
     '/Premiar_Florida_AM',
-    "/Premiar_florida_PM",
+    "/Premiar_Florida_PM",
     "/Premiar_New_York_AM",
     "/Premiar_New_York_PM",
+]
+
+Comandos_Resultados = [
     '/Obtener_Premiar_Florida_AM',
-    '/Obtener_Premiar_florida_PM',
+    '/Obtener_Premiar_Florida_PM',
     '/Obtener_Premiar_New_York_AM',
     '/Obtener_Premiar_New_York_PM'
 ]
@@ -37,8 +45,20 @@ def start(update,context):
 
 def info(update, context):
     user_id = update.effective_user['id']
-    logger.info(f'El usuario {user_id}, ha solicitado un numero random')
+    logger.info(f'El usuario {user_id}, ha solicitado ver informacion')
     message=Imprimir_Comandos(COMANDOS)
+    context.bot.sendMessage(chat_id= user_id, text=message)
+
+def Comandos_Resul(update, context):
+    user_id = update.effective_user['id']
+    logger.info(f'El usuario {user_id}, ha solicitado ver informacion')
+    message=Imprimir_Comandos(Comandos_Resultados)
+    context.bot.sendMessage(chat_id= user_id, text=message)
+
+def Comandos_Premiar(update, context):
+    user_id = update.effective_user['id']
+    logger.info(f'El usuario {user_id}, ha solicitado ver informacion')
+    message=Imprimir_Comandos(Comandos_Premios)
     context.bot.sendMessage(chat_id= user_id, text=message)
 
 def echo(update, context):
@@ -52,50 +72,58 @@ def echo(update, context):
 
 
 def florida_AM(update, context):
-    florida_am_Numeros = ORKAPI('Florida', "AM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Premiacion')
+    florida_am_Numeros = ORKAPI('Florida', "AM")
     logger.info(f'El usuario {user_id}, ha mandado a publicar los numeros FLORIDA AM')
     context.bot.sendMessage(chat_id= user_id, text=florida_am_Numeros)
 
 def florida_PM(update, context):
-    florida_am_Numeros = ORKAPI('Florida', "PM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Premiacion')
+    florida_am_Numeros = ORKAPI('Florida', "PM")
     logger.info(f'El usuario {user_id}, ha mandado a publicar los numeros FLORIDA PM')
     context.bot.sendMessage(chat_id= user_id, text=florida_am_Numeros)
 
 def New_York_AM(update, context):
-    New_York_numeros_am = ORKAPI('New York', "AM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Premiacion')
+    New_York_numeros_am = ORKAPI('New York', "AM")
     logger.info(f'El usuario {user_id}, ha mandado a publicar los numeros New York AM')
     context.bot.sendMessage(chat_id= user_id, text=New_York_numeros_am)
 
 def New_York_PM(update, context):
-    New_York_Numeros_PM = ORKAPI('New York', "PM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Premiacion')
+    New_York_Numeros_PM = ORKAPI('New York', "PM")
     logger.info(f'El usuario {user_id}, ha mandado a publicar los numeros FLORIDA PM')
     context.bot.sendMessage(chat_id= user_id, text=New_York_Numeros_PM)
 #?---------------------------------------------------------------------------------------
 def Obtener_florida_AM(update, context):
-    florida_am_Numeros = PROCESO('Florida', "AM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Buscar el Resultado')
+    florida_am_Numeros = PROCESO('Florida', "AM")
     logger.info(f'El usuario {user_id}, ha mandado a ver los numeros FLORIDA AM')
     context.bot.sendMessage(chat_id= user_id, text=florida_am_Numeros)
 
 def Obtener_florida_PM(update, context):
-    florida_am_Numeros = PROCESO('Florida', "PM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Buscar el Resultado')
+    florida_am_Numeros = PROCESO('Florida', "PM")
     logger.info(f'El usuario {user_id}, ha mandado a ver los numeros FLORIDA PM')
     context.bot.sendMessage(chat_id= user_id, text=florida_am_Numeros)
 
 def Obtener_New_York_AM(update, context):
-    New_York_numeros_am = PROCESO('New York', "AM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Buscar el Resultado')
+    New_York_numeros_am = PROCESO('New York', "AM")
     logger.info(f'El usuario {user_id}, ha mandado a ver los numeros New York AM')
     context.bot.sendMessage(chat_id= user_id, text=New_York_numeros_am)
 
 def Obtener_New_York_PM(update, context):
-    New_York_Numeros_PM = PROCESO('New York', "PM")
     user_id = update.effective_user['id']
+    context.bot.sendMessage(chat_id= user_id, text='Inicio el Proceso de Buscar el Resultado')
+    New_York_Numeros_PM = PROCESO('New York', "PM")
     logger.info(f'El usuario {user_id}, ha mandado a ver los numeros FLORIDA PM')
     context.bot.sendMessage(chat_id= user_id, text=New_York_Numeros_PM)
 
@@ -111,14 +139,16 @@ dp = updater.dispatcher
 #crear los manejadores
 dp.add_handler(CommandHandler('Start',start))
 dp.add_handler(CommandHandler('Info',info))
+dp.add_handler(CommandHandler('Premiar',Comandos_Premiar))
+dp.add_handler(CommandHandler('Ver_Resultados',Comandos_Resul))
 #?---------------------------------------------------------------
 dp.add_handler(CommandHandler('Premiar_Florida_AM',florida_AM))
-dp.add_handler(CommandHandler('Premiar_florida_PM',florida_PM))
+dp.add_handler(CommandHandler('Premiar_Florida_PM',florida_PM))
 dp.add_handler(CommandHandler('Premiar_New_York_AM',New_York_AM))
 dp.add_handler(CommandHandler('Premiar_New_York_PM',New_York_PM))
 #?----------------------------------------------------------------
 dp.add_handler(CommandHandler('Obtener_Premiar_Florida_AM',Obtener_florida_AM))
-dp.add_handler(CommandHandler('Obtener_Premiar_florida_PM',Obtener_florida_PM))
+dp.add_handler(CommandHandler('Obtener_Premiar_Florida_PM',Obtener_florida_PM))
 dp.add_handler(CommandHandler('Obtener_Premiar_New_York_AM',Obtener_New_York_AM))
 dp.add_handler(CommandHandler('Obtener_Premiar_New_York_PM',Obtener_New_York_PM))
 #?----------------------------------------------------------------
