a352aff4b6143215c21f28451e7ea06924393679
carlosdiazz <c.diazadriann@gmail.com>
2022-05-20 14:35:23 -0400

    Mas tiempo

--- Orkapi/DIARIO-DATOS.py
+++ Orkapi/DIARIO-DATOS.py
@@ -117,9 +117,9 @@ class Buscar():
             else:
                 self.intentos = self.intentos+1
                 intentos = self.intentos
-                if(self.intentos <= 10):
+                if(self.intentos <= 30):
                     print(f"\n\n\nNo se encontro esta loteria {loteria} con este sorteo: {sorteo}---------------------> Intento #{intentos}")
-                    time.sleep(150)
+                    time.sleep(60)
                     self.Buscar_Loteria()
 
 
