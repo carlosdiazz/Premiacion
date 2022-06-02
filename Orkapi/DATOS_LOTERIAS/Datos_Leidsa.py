50ee5b36ef66eeca57aba0f870860337a87e137c
carlosdiazz <c.diazadriann@gmail.com>
2022-06-01 22:19:40 -0400

    BOTON CERRAR LEIDSA

--- Orkapi/DATOS_LOTERIAS/Datos_Leidsa.py
+++ Orkapi/DATOS_LOTERIAS/Datos_Leidsa.py
@@ -5,9 +5,10 @@ Loto_Leidsa_Numbers_1 = '/html/body/div/section[2]/div/div[3]/div[3]/div/div[2]/
 Loto_Leidsa_Numbers_2 = '/html/body/div/section[2]/div/div[3]/div[3]/div/div[2]/table/tbody/tr[2]/td'
 Loto_Leidsa_Numbers_3 = '/html/body/div/section[2]/div/div[3]/div[3]/div/div[2]/table/tbody/tr[3]/td'
 
+BOTON_CERRAR = '/html/body/div[2]/div/div[1]/div/button'
 
 Leidsa_Todo = {
-    'URL' : [Loto_Leidsa_Pages, Loto_Leidsa_Resulado],
+    'URL' : [Loto_Leidsa_Pages, Loto_Leidsa_Resulado,BOTON_CERRAR],
     'FECHA' : [Loto_Leidsa_Fecha ],
     'NUMEROS' : [Loto_Leidsa_Numbers_1, Loto_Leidsa_Numbers_2, Loto_Leidsa_Numbers_3 ]
 }
