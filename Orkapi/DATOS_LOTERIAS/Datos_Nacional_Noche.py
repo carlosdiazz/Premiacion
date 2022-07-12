82d8599702e7df673dde90355b94499df6c92be6
carlosdiazz <c.diazadriann@gmail.com>
2022-07-12 10:33:29 -0400

    Arregle H=Ganamas y nacional

--- Orkapi/DATOS_LOTERIAS/Datos_Nacional_Noche.py
+++ Orkapi/DATOS_LOTERIAS/Datos_Nacional_Noche.py
@@ -1,9 +1,9 @@
 Loto_Nacional_Pages= 'https://loterianacional.gob.do/index.php'
 Loto_Nacional_Resulado = 'https://loterianacional.gob.do/index.php'
-Loto_Nacional_Fecha= '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/table[3]/tbody/tr[1]/td[2]/span'
-Loto_Nacional_Numbers_1 = '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/table[3]/tbody/tr[2]/td[1]/span'
-Loto_Nacional_Numbers_2 = '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/table[3]/tbody/tr[2]/td[2]/span'
-Loto_Nacional_Numbers_3 = '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/table[3]/tbody/tr[2]/td[3]/span'
+Loto_Nacional_Fecha= '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/table[3]/tbody/tr[1]/td[2]/span'
+Loto_Nacional_Numbers_1 = '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/table[3]/tbody/tr[2]/td[1]/span'
+Loto_Nacional_Numbers_2 = '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/table[3]/tbody/tr[2]/td[2]/span'
+Loto_Nacional_Numbers_3 = '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/table[3]/tbody/tr[2]/td[3]/span'
 
 
 Nacional_Todo = {
